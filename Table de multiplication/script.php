b940509174373c75ac4a46cee4b27f3ce429d641
ranya <amzghar.ranya.solicode@gmail.com>
2026-01-08 15:31:59 +0100

    Fix formatting and add newline at end of script.php

--- Table de multiplication/script.php
+++ Table de multiplication/script.php
@@ -2,13 +2,14 @@
 
 $x = 5;
 $y = 1;
-echo "Table de multiplication: ";
+echo "Table de multiplication: <br>";
 function multp($x, $y){
     return $x * $y;
 }
 for($y; $y<= 10; $y++){
     echo "$x * $y =".multp($x, $y)."<br>";
 }
+
 
 
 ?>
